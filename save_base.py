00812831b52709a9a61901b2d6eaa8c7d1bb3e7f
brwillian <wiliam-m-@hotmail.com>
2022-05-03 14:30:16 -0300

    updated project

---
+++ save_base.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from model.facemasknet import FaceMaskNet
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+
+class SaveBaseModel():
+    '''
+        Classe responsavel por salvar toda a base de dados contendo as matricas de cada face.
+    '''
+    def __init__(self, **kwargs):
+        super(SaveBaseModel, self).__init__()
+        self._directory = kwargs.get("directory") if kwargs.get("directory") else "dataset/"
+        self._model_path = kwargs.get("path_model") if kwargs.get("path_model") else "weights/facemasknet.h5"
+        self._train_generator = self._load_datagen()
+        self._model = FaceMaskNet()
+        self._classes_names = []
+        self._db = []
+
+    def _load_datagen(self):
+        '''
+            Carregar a base de imagens.
+        '''
+
+        train_datagen = ImageDataGenerator(rescale=1. / 255)
+
+        train_generator = train_datagen.flow_from_directory(
+            self._directory,
+            target_size=(227, 227),
+            batch_size=1,
+            class_mode='sparse'
+        )
+
+        return train_generator
+
+    def get_db_embeddings(self):
+        '''
+            Faz o predição das imagens, carrega e rotula todo o dataset.
+        '''
+        classes_names = self._get_class_name()
+        predicted = self._load_model_facemasknet()
+
+        for _class, _embeddings in zip(self._train_generator, predicted):
+            _class = list(_class[1])
+            _embeddings = list(_embeddings)
+            self._db.append([classes_names[int(*_class)], int(*_class), *_embeddings])
+
+        return pd.DataFrame(self._db)
+
+    def _get_class_name(self):
+        '''
+            Pega todos os nomes baseadas em pastas.
+        '''
+        for _, dir, _ in os.walk(self._directory):
+            for path in sorted(dir):
+                self._classes_names.append(path)
+
+        return self._classes_names
+
+    def save_embeddings(self):
+        '''
+             Salva em tsv
+        '''
+        df = self.get_db_embeddings()
+        df.to_csv('bd.tsv', sep='\t', index=False)
+
+    def _load_model_facemasknet(self):
+        '''
+            Carrega o modelo e faz a predição.
+        '''
+        self._model.load_weights(filepath=self._model_path)
+
+        results = self._model.predict(self._train_generator)
+
+        return results
+
+
+if __name__ == "__main__":
+    SaveBaseModel = SaveBaseModel(directory='/home/willian/Downloads/post-processed/', path_model='weights/facemasknet.h5')
+    SaveBaseModel.get_db_embeddings()
+    SaveBaseModel.save_embeddings()
--- train.py
+++ train.py
@@ -1,44 +1,44 @@
-import numpy as np
-import io
 from model.facemasknet import FaceMaskNet
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow_addons as tfa
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
 
 train_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     '/home/willian/Downloads/post-processed',
     target_size=(227, 227),
-    batch_size=64,
+    batch_size=32,
     class_mode='sparse'
 )
 
 if __name__ == "__main__":
-
     strategy = tf.distribute.MirroredStrategy()
     print('Número de gpus: {}'.format(strategy.num_replicas_in_sync))
+
+    callbacks = [ModelCheckpoint(
+        'weights/facemasknet.h5',
+        monitor='loss',
+        save_best_only=True,
+        save_freq="epoch",
+        save_weights_only=False
+    )]
+
+    optimizer = Adam()
 
     if strategy.num_replicas_in_sync > 1:
         with strategy.scope():
             model = FaceMaskNet()
             model.summary()
-            model.compile(optimizer='Adam', loss=tfa.losses.TripletHardLoss())
+            model.compile(optimizer=optimizer, loss=tfa.losses.TripletHardLoss())
     else:
         model = FaceMaskNet()
         model.summary()
-        model.compile(optimizer='Adam', loss=tfa.losses.TripletHardLoss())
+        model.compile(optimizer=optimizer, loss=tfa.losses.TripletHardLoss())
 
-    model.fit(train_generator, epochs=1)
-
-    results = model.predict(train_generator)
-
-    np.savetxt("vetor.tsv", results, delimiter='\t')
-
-    out_m = io.open('meta.tsv', 'w', encoding='utf-8')
-    for _, labels in train_generator:
-        [out_m.write(str(x)) for x in labels]
-    out_m.close()
+    model.fit(train_generator, epochs=20, callbacks=callbacks)
 
 
 
