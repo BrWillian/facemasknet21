9067df5696e5f1d904fffc73f9cc30c2d2711558
brwillian <wiliam-m-@hotmail.com>
2022-05-03 02:09:37 -0300

    generated train base

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 import numpy as np
-import os
-import cv2
-from model.facemasknet21 import FaceMaskNet
+import io
+from model.facemasknet import FaceMaskNet
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow_addons as tfa
 
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 
-def list_dir(dir):
-    data = []
-    for _, _, files in os.walk(dir):
-        for file in files:
-            data.append(file)
-
-    data = list(map(lambda s: s.split("")[0], data))
-    return data
-
-
-def train_generator(**kwargs):
-    directory = kwargs.get('directory') if kwargs.get('directory') else 'input/train'
-    data = sorted(list_dir(directory))
-    batchSize = kwargs.get('batch_size') if kwargs.get('batch_size') else 16
-
-    while True:
-        for start in range(0, len(data), batchSize):
-            x_batch = []
-            end = min(start + batchSize, len(data))
-            id_train_batch = data[start:end]
-            for id in id_train_batch:
-                im = cv2.imread(directory + '{}.jpg'.format(id))
-                im = cv2.resize(im, (227, 227, 3))
-                x_batch.append(im)
-
-            x_batch = np.array(x_batch, np.float32) / 255
-            yield x_batch
-
+train_generator = train_datagen.flow_from_directory(
+    '/home/willian/Downloads/post-processed',
+    target_size=(227, 227),
+    batch_size=64,
+    class_mode='sparse'
+)
 
 if __name__ == "__main__":
-    model = FaceMaskNet()
-    print(model.summary())
+
+    strategy = tf.distribute.MirroredStrategy()
+    print('Número de gpus: {}'.format(strategy.num_replicas_in_sync))
+
+    if strategy.num_replicas_in_sync > 1:
+        with strategy.scope():
+            model = FaceMaskNet()
+            model.summary()
+            model.compile(optimizer='Adam', loss=tfa.losses.TripletHardLoss())
+    else:
+        model = FaceMaskNet()
+        model.summary()
+        model.compile(optimizer='Adam', loss=tfa.losses.TripletHardLoss())
+
+    model.fit(train_generator, epochs=1)
+
+    results = model.predict(train_generator)
+
+    np.savetxt("vetor.tsv", results, delimiter='\t')
+
+    out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+    for _, labels in train_generator:
+        [out_m.write(str(x)) for x in labels]
+    out_m.close()
+
+
+
